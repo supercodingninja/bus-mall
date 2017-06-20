0a54cf9c98809390440c061455a3cd435294f8a6
Frederick D. Thomas, Jr.; Rev., USMC OEF/OIF I/OIF II Vet <frederickdthomasjr@gmail.com>
2017-06-20 08:59:42 -0700

    Add files via upload

---
+++ bus-mall.js
@@ -0,0 +1,29 @@
+'use strict';
+
+// look up how to connect to folder of images. //
+var imagesOption = [];
+
+// ref. https://stackoverflow.com/questions/24029767/returning-a-random-new-initialized-object-in-javascript //
+function A() {
+  this.option = "A";
+}
+
+function B() {
+  this.option = "B";
+}
+
+function C() {
+  this.option = "C";
+}
+
+var items = [A, B, C];
+
+function getRandomItem() {
+  var rand = Math.floor(Math.random() * items.length);
+  return new items[rand]();
+}
+
+log(getRandomItem());
+log(getRandomItem());
+log(getRandomItem());
+log(getRandomItem());
