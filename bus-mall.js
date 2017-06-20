70cdbb848a091a550873e4d8b487796178fb963f
Frederick Thomas <supercodingninja@outlook.com>
2017-06-20 15:08:17 -0700

    Corrections

--- bus-mall.js
+++ bus-mall.js
@@ -1,7 +1,7 @@
 'use strict';
 
 // look up how to connect to folder of images. //
-var imagesOption = [];
+var option = [];
 
 // ref. https://stackoverflow.com/questions/24029767/returning-a-random-new-initialized-object-in-javascript //
 function A() {
@@ -16,47 +16,30 @@ function C() {
   this.option = "C";
 }
 
-var items = [A, B, C];
+var allOptions = [A, B, C];
 
 function render([A, B, C];) {
-  for(var i = 0; i < allStores.length; i++) {
-    threeOptions[i].render(3);
+  for(var i = 0; i < allOptions.length; i++) {
+    allOptions[i].render(3);
   }
 
-new Option('', '', '');
-new Option('', '', '');
-new Option('', '', '');
-new Option('', '', '');
-new Option('', '', '');
-new Option('', '', '');
-new Option('', '', '');
-new Option('', '', '');
-new Option('', '', '');
-new Option('', '', '');
-new Option('', '', '');
-new Option('', '', '');
-new Option('', '', '');
-new Option('', '', '');
-new Option('', '', '');
-new Option('', '', '');
-new Option('', '', '');
-new Option('', '', '');
-new Option('', '', '');
-new Option('', '', '');
-new Option('', '', '');
-new Option('', '', '');
-new Option('', '', '');
-new Option('', '', '');
-new Option('', '', '');
+new option('img/bathroom.jpg', 'img/boots.jpg', 'img/breakfast-1.jpg');
+new option('img/breakfast.jpg', 'img/bubblegum.jpg', 'img/chair.jpg');
+new option('img/cthulhu.jpg', 'img/dog-duck.jpg', 'img/dragon.jpg');
+new option('img/pen.jpg', 'img/pet-sweep.jpg', 'img/scissors.jpg');
+new option('img/shark.jpg', 'img/sweep.png', 'img/tauntaun.jpg');
+new option('img/unicorn.jpg', 'img/usb.gif', 'img/water-can.jpg');
+new option('img/wine-glass.jpg');
+
 
 };
 
-function getRandomItem() {
-  var rand = Math.floor(Math.random() * items.length);
-  return new items[rand]();
+function getRandomOptions() {
+  var rand = Math.floor(Math.random() * allOptions.length);
+  return new option[rand]();
 }
 
-log(getRandomItem());
-log(getRandomItem());
-log(getRandomItem());
-log(getRandomItem());
+log(getRandomOptions());
+log(getRandomOptions());
+log(getRandomOptions());
+log(getRandomOptions());
