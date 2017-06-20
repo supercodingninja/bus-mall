ea88aaf81ee12bbde6e83806b2b76c3c49d1673f
Frederick Thomas <supercodingninja@outlook.com>
2017-06-20 16:56:39 -0700

    Corrections

--- bus-mall.js
+++ bus-mall.js
@@ -1,45 +1,52 @@
 'use strict';
 
-// look up how to connect to folder of images. //
-var option = [];
+// ref. 'Returning a random new initialized object in JavaScript'
+// https://stackoverflow.com/
+// https://stackoverflow.com/questions/24029767/returning-a-random-new-initialized-object-in-javascript
+var option = function renderOption() {
 
-// ref. https://stackoverflow.com/questions/24029767/returning-a-random-new-initialized-object-in-javascript //
-function A() {
-  this.option = "A";
-}
-
-function B() {
-  this.option = "B";
-}
-
-function C() {
-  this.option = "C";
-}
-
-var allOptions = [A, B, C];
-
-function render([A, B, C];) {
   for(var i = 0; i < allOptions.length; i++) {
     allOptions[i].render(3);
-  }
 
-new option('img/bathroom.jpg', 'img/boots.jpg', 'img/breakfast-1.jpg');
-new option('img/breakfast.jpg', 'img/bubblegum.jpg', 'img/chair.jpg');
-new option('img/cthulhu.jpg', 'img/dog-duck.jpg', 'img/dragon.jpg');
-new option('img/pen.jpg', 'img/pet-sweep.jpg', 'img/scissors.jpg');
-new option('img/shark.jpg', 'img/sweep.png', 'img/tauntaun.jpg');
-new option('img/unicorn.jpg', 'img/usb.gif', 'img/water-can.jpg');
-new option('img/wine-glass.jpg');
+// need to look up how to connect to folder of images. //
+  if option = function A() {
+    this.option = "A";
+    }
 
+    else if (false) {
+      option = function B() {
+      this.option = "B";
+      }
+    }
 
-};
+    else if (false) {
+    option = function C() {
+    this.option = "C";
+      }
+    }
 
-function getRandomOptions() {
-  var rand = Math.floor(Math.random() * allOptions.length);
-  return new option[rand]();
+  new option('img/bathroom.jpg', 'img/boots.jpg', 'img/breakfast-1.jpg');
+  new option('img/breakfast.jpg', 'img/bubblegum.jpg', 'img/chair.jpg');
+  new option('img/cthulhu.jpg', 'img/dog-duck.jpg', 'img/dragon.jpg');
+  new option('img/pen.jpg', 'img/pet-sweep.jpg', 'img/scissors.jpg');
+  new option('img/shark.jpg', 'img/sweep.png', 'img/tauntaun.jpg');
+  new option('img/unicorn.jpg', 'img/usb.gif', 'img/water-can.jpg');
+  new option('img/wine-glass.jpg');
+
 }
 
-log(getRandomOptions());
+  tdEl = document.createElement('td');
+  tdEl.textContent = allOptions;
+  trEl.appendChild(tdEl);
+  theTable.appendChild(trEl);
+
+  function getRandomOptions() {
+    var rand = Math.floor(Math.random() * allOptions.length);
+    return new option[rand]();
+  }
+}
+
+renderOption(getRandomOptions());
 log(getRandomOptions());
 log(getRandomOptions());
 log(getRandomOptions());
