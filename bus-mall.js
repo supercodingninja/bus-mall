572886e5650a685ca9551bdb9fc8ebf354f20848
Frederick Thomas <supercodingninja@outlook.com>
2017-06-21 06:49:41 -0700

    Additions and Corrections

--- bus-mall.js
+++ bus-mall.js
@@ -1,40 +1,51 @@
 'use strict';
 
-// ref. 'Returning a random new initialized object in JavaScript'
-// https://stackoverflow.com/
-// https://stackoverflow.com/questions/24029767/returning-a-random-new-initialized-object-in-javascript
 var userChoice = function renderOption() {
 
   var clickTotal = 0
   var option = []
-  var newOption
 
-  for(var i = 0; i < allOptions.length; i++) {
-    allOptions[i].render(3);
+  // for(var i = 0; i < allOptions.length; i++) {
+  // allOptions[i].render(3); NO-NO-NO-NO-N0! DO SOMETHING DANGEROUS: THINK!!! //
+  var container = document.getElementById('option');
 
-// need to look up how to connect to folder of images. //
-  var A = [0];
-  var B = [1];
-  var C = [2];
+  container.addEventListener('click', handleClick);
 
-  if option = function A() {
+  function handleClick(event) {
+    console.log(event.target.id);
+
+    if(event.target.id === 'option') {
+      alert('Thank you.  Please make your next choice.' )
+    }
+  }
+
+// ref. 'Returning a random new initialized object in JavaScript'
+// https://stackoverflow.com/
+// https://stackoverflow.com/questions/24029767/returning-a-random-new-initialized-object-in-javascript
+  var A = document.getElementById('A');
+  var B = document.getElementById('B');
+  var C = document.getElementById('C');
+  var Skip = document.getElementById('Skip');
+
+  if A = function A() {
     this.option = "A";
     }
     option.console.log();
 
     else if (false) {
-      option = function B() {
+      B = function B() {
       this.option = "B";
       }
     }
     option.console.log();
 
     else if (false) {
-    option = function C() {
+    C = function C() {
     this.option = "C";
       }
     }
 
+  // need to look up how to connect to folder of images. //
   new option('img/bathroom.jpg', 'img/boots.jpg', 'img/breakfast-1.jpg');
   new option('img/breakfast.jpg', 'img/bubblegum.jpg', 'img/chair.jpg');
   new option('img/cthulhu.jpg', 'img/dog-duck.jpg', 'img/dragon.jpg');
@@ -42,8 +53,6 @@ var userChoice = function renderOption() {
   new option('img/shark.jpg', 'img/sweep.png', 'img/tauntaun.jpg');
   new option('img/unicorn.jpg', 'img/usb.gif', 'img/water-can.jpg');
   new option('img/wine-glass.jpg');
-
-}
 
   option.prototype.render = function() {
    this.allOptions();
@@ -55,8 +64,8 @@ var userChoice = function renderOption() {
   function getRandomOptions() {
     var rand = Math.floor(Math.random() * allOptions.length);
     return new option[rand]();
+    }
   }
 }
-
 renderOption(getRandomOptions());
 log(getRandomOptions());
